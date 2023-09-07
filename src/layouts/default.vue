34b754f37f6df6ce56526a9027521eed898bece5
unknown <oscar.bravo@tismart.com>
2023-09-07 01:25:40 -0500

    initial commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: true,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+html{
+  overflow: hidden;
+}
+.container--fluid{
+  padding: 0 15px 0 15px;
+  margin: 0 auto 0 auto;
+  height: 100%;
+}
+</style>
